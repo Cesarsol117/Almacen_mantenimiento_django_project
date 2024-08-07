ef01c8f859010fc27fc696f451b0b29645bdcb67
Cesarsol117 <cesar_p117@hotmail.com>
2024-08-07 10:56:05 -0500

    con codigo jinja se cargan los staticos

--- almacen_mantenimiento/settings.py
+++ almacen_mantenimiento/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# modifiamos los archivos staicos o css y js
+STATIC_URL = '/static/' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

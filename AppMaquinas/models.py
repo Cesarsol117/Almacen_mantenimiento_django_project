a768d88ed2ff5bfaf6008ff6ae74d522186d2890
Cesarsol117 <cesar_p117@hotmail.com>
2024-10-01 18:57:39 -0500

    create model Machine

--- AppMaquinas/models.py
+++ AppMaquinas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Machine(models.Model):
+    name = models.TextField(max_length=200, verbose_name="nombre")
+    description = models.TextField(max_length=500, verbose_name="descripcion")
+    photo = models.ImageField(upload_to='logos', null=True, blank=True, verbose_name='Foto')
+    
+    def __str__(self):
+        return self.name
+    
--- almacen_mantenimiento/settings.py
+++ almacen_mantenimiento/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'AppInsumos',
     'AppHerramientas',
     'AppUsers',
+    'AppMaquinas',
     
 ]
 
